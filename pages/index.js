a87d7a124ce271a5eb4e61895d3bf9cabe9619cc
Mac <mac@Mac.local>
2020-11-09 18:50:03 +0100

    primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import{ useState } from 'react' ;
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <contador />
+        </div>
+    )
+}
+function contador(){
+    const [contador,setcontador] = useState(1);
+
+    function AdicionarContador() {
+        setcontador(contador + 1)
+    }
+
+    return
+    <div>
+        <div>{contador}</div>
+        <button onClick={AdicionarContador}>Adicionar</button>
+    </div>
+}
+
+export default Home
